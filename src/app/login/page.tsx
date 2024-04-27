96aa0686d3e0d853a99eb2edb04a1e65ded4c340
gurungsandesh <lastglader@gmail.com>
2024-04-27 20:06:17 +0545

    feat: adds shadcn package with login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,82 @@
-import { login, signup } from "./actions";
+"use client"
+
+import logo from "@/assets/Logo.png"
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from '@/components/ui/input'
+import { zodResolver } from "@hookform/resolvers/zod"
+import Image from 'next/image'
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const loginschema = z.object({
+  username: z.string().nonempty({ message: 'Username is requried' }),
+  password: z.string().nonempty({ message: 'Password is requried' }),
+})
 
 export default function LoginPage() {
-  return (    
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-    <form className="flex flex-col items-start justify-center p-6 gap-4">
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required className="border-2 border-gray-300 rounded-md p-2 mb-4" />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required className="border-2 border-gray-300 rounded-md p-2 mb-4" />
-      <button formAction={login} className="w-full bg-blue-500 text-white px-4 py-2 rounded-md">
-        Log in
-      </button>
-      <button formAction={signup} className="w-full bg-green-500 text-white px-4 py-2 rounded-md">
-        Sign up
-      </button>
-    </form>
-  </main>
-  );
+
+  const loginform = useForm<z.infer<typeof loginschema>>({
+    resolver: zodResolver(loginschema),
+    defaultValues: {
+      username: "",
+      password: ""
+    }
+  })
+
+  function onSubmit(values: z.infer<typeof loginschema>) {
+    console.log("form values", values)
+  }
+
+  return (
+
+    <div className="p-5 flex flex-col gap-5 h-screen" >
+      <div className=" grid place-items-center">
+        <Image src={logo} alt="Logo"></Image>
+      </div>
+
+      <Form {...loginform}>
+        <form onSubmit={loginform.handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-5 ">
+            <FormField
+              control={loginform.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={loginform.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+
+
+
+          </div>
+        </form>
+      </Form>
+
+    </div >
+  )
 }
