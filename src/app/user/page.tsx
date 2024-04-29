af98b414efdce83581dd0a8edc010605f20ff3b4
gurungsandesh <lastglader@gmail.com>
2024-04-29 22:28:11 +0545

    feat: added user dashboard pages

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,22 +1,146 @@
-import React from 'react'
+"use client"
+
+import logo from "@/assets/Logo.png";
+import heroImage from "@/assets/hero_graphic.png";
+import { Button } from "@/components/ui/button";
+import { Input } from '@/components/ui/input';
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
+import Image from 'next/image';
+import Link from "next/link";
+import { RiTeamFill, RiUserFill } from "react-icons/ri";
+import { TbSoccerField } from "react-icons/tb";
+
+//#20C980
+// #0B7C2A
+
+
+// #14D045
+// #08985A
 
 const UserDashboard = () => {
     return (
-        <div className='h-screen bg-red-500 relative'>
-            This is user page
+        <div className=' bg-hero-cover bg-no-repeat bg-cover w-screen h-screen overflow-x-hidden ' >
+            <div className='flex justify-between px-10 py-3'>
+                <Image src={logo} alt="Logo" className=" h-6 sm:h-[50px] " ></Image>
 
-            <div className=' bg-[#08985A] absolute bottom-0 left-0 right-0 h-[10vh] '>
+                <NavigationMenu className=" invisible sm:visible ">
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <Link href="/docs" legacyBehavior passHref>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    Home
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <Link href="/docs" legacyBehavior passHref>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    settings
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <Link href="/docs" legacyBehavior passHref>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    Matches
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+            <div className='flex flex-col items-center relative '>
+                <Image src={heroImage} alt='hero image' height={450}></Image>
+                <div className=' flex gap-3 items-start sm:w-[50vw] absolute top-[60%] '>
+                    <Input placeholder="Futsal near me" />
+                    <Button className="bg-[#08985A] hover:bg-[#14D045] hidden sm:block" >
+                        Search
+                    </Button>
+                </div>
 
-                <div className='h-[10vh] w-[10vh] bg-green-300 absolute bottom-10 left-[50%] -translate-x-1/2 overflow-hidden flex flex-col justify-between rounded-[50%]'>
-                    <div className='cursor-pointer'>
-                        test
+            </div>
+            <div className='relative flex flex-col items-center mt-14 gap-14 ' >
+                <div className='flex justify-around w-[70%] h-[22vh] items-center'>
+                    <div className='rounded-[100%] bg-[#20C980] flex flex-col items-center justify-center gap-1 cursor-pointer hover:-translate-y-3 duration-500 hover:bg-[#08985A] h-[80px] w-[80px] sm:h-[130px] sm:w-[130px]'>
+                        <RiTeamFill className=' text-md sm:text-lg text-gray-200 ' />
+                        <span className='text-md sm:text-lg text-gray-200  '>
+                            Team
+                        </span>
                     </div>
-                    <div className='bg-slate-200 h-2 w-screen'>
+                    <div className='rounded-[100%] self-end bg-[#20C980] flex flex-col items-center justify-center gap-1 cursor-pointer hover:-translate-y-3 duration-500 hover:bg-[#08985A]
+                    h-[80px] w-[80px] sm:h-[130px] sm:w-[130px]' >
+                        <RiUserFill className='text-md sm:text-lg  text-gray-200 ' />
+                        <span className='text-md sm:text-lg  text-gray-200 '>
+                            Profile
+                        </span>
                     </div>
-                    <div className='cursor-pointer' >
-                        check
+                    <div className='rounded-[100%] bg-[#20C980] flex flex-col items-center justify-center gap-1 cursor-pointer hover:-translate-y-3 duration-500 hover:bg-[#08985A] h-[80px] w-[80px] sm:h-[130px] sm:w-[130px]'>
+                        <RiTeamFill className='text-md sm:text-lg text-gray-200 ' />
+                        <span className='text-md sm:text-lg text-gray-200  '>
+                            Matches
+                        </span>
                     </div>
                 </div>
+
+
+                <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+                    <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
+                        <button data-tooltip-target="tooltip-home" type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                            <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                            </svg>
+                            <span className="sr-only">Home</span>
+                        </button>
+                        <div id="tooltip-home" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                            Home
+                            <div className="tooltip-arrow" data-popper-arrow></div>
+                        </div>
+                        <button data-tooltip-target="tooltip-wallet" type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                            <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z" />
+                                <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z" />
+                            </svg>
+                            <span className="sr-only">Wallet</span>
+                        </button>
+                        <div id="tooltip-wallet" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                            Wallet
+                            <div className="tooltip-arrow" data-popper-arrow></div>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <button data-tooltip-target="tooltip-new" type="button" className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+                                <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                                <span className="sr-only">New item</span>
+                            </button>
+                        </div>
+                        <div id="tooltip-new" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                            Create new item
+                            <div className="tooltip-arrow" data-popper-arrow></div>
+                        </div>
+                        <button data-tooltip-target="tooltip-settings" type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                            <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
+                            </svg>
+                            <span className="sr-only">Settings</span>
+                        </button>
+                        <div id="tooltip-settings" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                            Settings
+                            <div className="tooltip-arrow" data-popper-arrow></div>
+                        </div>
+                        <button data-tooltip-target="tooltip-profile" type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                            <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                            </svg>
+                            <span className="sr-only">Profile</span>
+                        </button>
+                        <div id="tooltip-profile" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                            Profile
+                            <div className="tooltip-arrow" data-popper-arrow></div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -32,9 +32,13 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        "hero-image": "url('/assets/hero_graphic.png')",
+        "hero-cover": "url('/cover.png')",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
